518c84f6a64fb7c996ffc23f1c9de54d547de7cb
vuyanitutwane <vuyaitutwane@outlook.com>
2020-02-17 10:34:18 +0200

    modified Program.cs. It noow prints my student number

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,7 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("g7t4564");
         }
     }
 }
